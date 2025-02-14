abf739bc1fde6bf7d1186e053dc44486f9c446f0
Biruk_Afework <140178047+Biruk2580@users.noreply.github.com>
2025-02-13 18:14:56 -0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,7 +4,6 @@ $databaseHost = 'localhost';
 $databaseName = 'contact_test';
 $databaseUsername = 'root';
 $databasePassword = '';
-
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 	
 ?>
